246107eb3dcd180606fbee51f61bb9d82e188f3c
Jefferson Griebel <jefferson.griebel@gmail.com>
2021-07-14 14:06:17 -0400

    updated the date to display month, year, and day instead of a timestamp

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -4,7 +4,11 @@ function ExpenseItem(props) {
   
     return (
         <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+            <div>
+              <div>{props.date.toLocaleString('en-US', {month: 'long'})}</div>
+              <div>{props.date.toLocaleString('en-US', {year: 'numeric'})}</div>
+              <div>{props.date.toLocaleString('en-US', {day: 'numeric'})}</div>
+            </div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
